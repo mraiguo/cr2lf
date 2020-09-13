f85c6e7e1e6422ac709a38f9a560a7a696bdff03
mraiguo <810158465@qq.com>
2020-09-13 13:59:16 +0800

    '文档，参数'

--- index.js
+++ index.js
@@ -6,20 +6,29 @@ const fg = require('fast-glob')
 const {
   crlf, chkcrlf, LF
 } = require('crlf-normalize')
+const { Command } = require('commander')
+const program = new Command()
+
+program
+  .option('-i, --ignore <filepath>', '忽略检查的文件')
+
+program.parse(process.argv)
+
+console.log(program.ignore)
 
 function checkLRForBinFile() {
-  console.log(chalk.yellow('文件夹下的除了 node_modules 外的转为 LF 行尾符'))
-  console.log('开始检查')
+  console.log(chalk.yellow(`文件夹下的除了 node_modules ${program.ignore} 外的转为 LF 行尾符`))
   fg.sync(
-    ['**/*.*'],
-    { ignore: ['**/node_modules'] }
+    ['**/*.js'],
+    { ignore: ['**/node_modules', program.ignore] }
   ).forEach((file) => {
-    const filePath = path.resolve(__dirname, file)
+    const filePath = path.resolve(process.cwd(), file)
     const fileContent = fs.readFileSync(filePath, {
       encoding: 'utf-8'
     })
+
     if (chkcrlf(fileContent).crlf) {
-      console.error('Bin入口文件', filePath, '为CRLF, 将自动修正')
+      console.error('文件', filePath, '为CRLF, 将自动修正')
       const fixedContent = crlf(fileContent, LF)
       fs.writeFileSync(filePath, fixedContent, {
         encoding: 'utf-8'
@@ -29,4 +38,4 @@ function checkLRForBinFile() {
   console.log('检查完成')
 }
 
-checkLRForBinFile()
+module.exports = checkLRForBinFile
